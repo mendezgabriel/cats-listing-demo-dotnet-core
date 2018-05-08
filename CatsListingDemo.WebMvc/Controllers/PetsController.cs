b5600794789b4359c5b1ba51210cce8d72dce2d9
Gabriel Mendez <mendezbytwo@hotmail.com>
2018-05-08 11:24:55 +1000

    Added regular Core MCV UI and resolved dependencies.

---
+++ CatsListingDemo.WebMvc/Controllers/PetsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatsListingDemo.BusinessInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatsListingDemo.WebMvc.Controllers
+{
+    public class PetsController : Controller
+    {
+        private IPetOwnerProcessor _petOwnerProcessor;
+
+        public PetsController(IPetOwnerProcessor petOwnerProcessor)
+        {
+            _petOwnerProcessor = petOwnerProcessor;
+        }
+
+        // GET: Pets
+        public ActionResult Index()
+        {
+            var result = _petOwnerProcessor.GetPetsByGender();
+            return View();
+        }
+
+        // GET: Pets/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pets/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pets/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Pets/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pets/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Pets/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
